3084f9c4499ac7d6aa28f839074433af95fa7f47
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2024-06-27 18:00:56 +0530

    message

--- src/service/research/journal-article-service.ts
+++ src/service/research/journal-article-service.ts
@@ -1,5 +1,7 @@
 import { getLogger } from '$config/logger-context';
-import { getJournalArticlePublished, insertJournalArticleModel } from '$model/journal-article-model';
+import { getJournalArticlePublished, insertJournalArticleModel, updateJournalArticleModel,
+    deleteJournalArticleModel
+ } from '$model/journal-article-model';
 import { paginationDefaultType } from 'types/db.default';
 
 import { journalArticleDetails } from 'types/research.types';
@@ -34,4 +36,31 @@ export const insertJournalArticleService = async (journalDetails: journalArticle
     const data = await insertJournalArticleModel(journalDetails);
  
     return data;
+ }; 
+
+
+ export const updateJournalArticleService = async (updateJournalDetails: journalArticleDetails) => {
+    const logger = getLogger();
+    logger.info('INSIDE GET SUBJECT JOURNAL ARTICLE  SERVICES');
+ 
+    const data = await updateJournalArticleModel(updateJournalDetails);
+ 
+    return data;
  };
+
+
+ export const deleteJournalArticleService = async(journalPaperId : number) => {
+    const logger = getLogger();
+    logger.info('INSIDE GET SUBJECT JOURNAL ARTICLE  SERVICES');
+
+    console.log('journalPaperId in service ===>>>', journalPaperId);
+ 
+    const data = await deleteJournalArticleModel(journalPaperId);
+
+    console.log('data ===>>>>>', data);
+    return {
+        status : data.status,
+        message : data.message
+    };
+
+ }
