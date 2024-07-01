7080dd8128ed25db0d1300e1135803ecaea0b2e9
avinash-kote <avinash.kote.EXT@nmims.edu>
2024-07-01 11:20:15 +0530

    message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,7 @@ import bookPublicationRoutes from './research-routes/book-publication.routes'
 import editedbookRoutes from './research-routes/edited-book-routes';
 import bookChapterRoutes from './research-routes/book-chapter-routes';
 import confereRoutes from './research-routes/conference-routes';
+import eContentRoute from './research-routes/e-content-routes'
 
 //ankit
 
@@ -24,6 +25,7 @@ router.use('/', bookPublicationRoutes);
 router.use('/', editedbookRoutes);
 router.use('/', bookChapterRoutes);
 router.use('/', confereRoutes);
+router.use('/', eContentRoute);
 
 
 export default router; 
--- types/research.types.ts
+++ types/research.types.ts
@@ -122,3 +122,34 @@ export type journalArticleDetails = {
    conference_documents: string[];
    award_documents: string[];
  }
+
+ export type IPRDetails = {
+   
+   ipr_id:number;
+   title: string;
+   appln_no: number
+   filed_date:string;
+   grant_date:string;  
+   published_date: string;
+   publication_no: number;
+   granted_no: number;
+   institue_affiliation: string;
+   patent_status: number;
+   invention_type : number[];
+   sdg_goals : number[];
+   applicant_names :number[];
+   nmims_school : string[];
+   nmims_campus:string[];  
+   inventors_id: number[];
+   supporting_documents:string[];
+}
+
+export type EContent = {
+   faculty_name: string;
+   module: string;
+   module_platform: string;
+   launching_date: string;
+   document_link: string;
+   media_link: string;
+   facilitylist: string;
+}
